136a5119cfa40386873d572e10a5ba54407353fb
Utkarsh Mishra <mr.utkarsh.mishra@gmail.com>
2019-08-21 20:11:33 +0530

    Changes in setting.py

--- autocompleteapi/views.py
+++ autocompleteapi/views.py
@@ -11,6 +11,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Search
 
+# opening file
 file = open(os.getcwd() + "/word_search.tsv", "r")
 global data
 data = []
@@ -18,20 +19,22 @@ for i in file.read().split("\n"):
     ele = i.split('\t')
     if len(ele) == 2:
         data.append((int(ele[1]), ele[0]))
-data.sort()
 
+#sorting the data based on their usage
+data.sort()
 data = [x for _, x in data]
 
 def index(request):
-   template = loader.get_template('index.html') # getting our template
+   # getting our template
+   template = loader.get_template('index.html')
    return HttpResponse(template.render())
 
 class SearchView(APIView):
 
-
+    # Function that find match through regular expression matching
     def fuzzyfinder(self, user_input, collection):
         suggestions = []
-        pattern = '.*?'.join(user_input)   # Converts 'djm' to 'd.*?j.*?m'
+        pattern = '.*?'.join(user_input)   # Converts 'utk' to 'u.*?t.*?k'
         regex = re.compile(pattern)  # Compiles a regex.
         for item in collection:
             match = regex.search(item)   # Checks if the current item matches the regex.
@@ -39,13 +42,15 @@ class SearchView(APIView):
                 suggestions.append((len(match.group()), match.start(), item))
         return [x for _, _, x in sorted(suggestions)]
 
-
+    # perform get request of api
     def get(self, request):
         global data
         renderer_classes = [TemplateHTMLRenderer]
         template_name = 'index.html'
         if 'word' in request.GET:
-            result = "</br>".join(self.fuzzyfinder(user_input=request.GET['word'],collection=data))
+            result = self.fuzzyfinder(user_input=request.GET['word'],collection=data)
+
         else:
             result = None
-        return Response(result)
+        # repond to get request
+        return HttpResponse(json.dumps(result), content_type="application/json")
